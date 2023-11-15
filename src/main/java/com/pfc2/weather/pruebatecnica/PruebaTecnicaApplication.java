f585120110eada6051467f0ec4c9b941855e87de
egonzalez27 <egonzalez@lendingpoint.com>
2023-11-14 21:19:33 -0600

    Adding open api

--- src/main/java/com/pfc2/weather/pruebatecnica/PruebaTecnicaApplication.java
+++ src/main/java/com/pfc2/weather/pruebatecnica/PruebaTecnicaApplication.java
@@ -1,13 +1,23 @@
 package com.pfc2.weather.pruebatecnica;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PruebaTecnicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaTecnicaApplication.class, args);
+	}
+	@Bean
+	public GroupedOpenApi groupedOpenApi(){
+		return GroupedOpenApi.builder()
+				.group("api")
+				.group("sys")
+				.packagesToScan("com.pfc2.weather.pruebatecnica.controller")
+				.build();
 	}
 
 }
